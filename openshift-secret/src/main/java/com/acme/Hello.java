1f0d88ee303b8c62add4a4c92fd3dbd08a954f0d
Leticia Konno <lkonno@redhat.com>
2022-12-07 09:37:00 -0300

    Creating project to test secret

---
+++ openshift-secret/src/main/java/com/acme/Hello.java
@@ -0,0 +1,16 @@
+package com.acme;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class Hello {
+    private final String content;
+
+    public Hello(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
